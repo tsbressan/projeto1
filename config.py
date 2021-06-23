527d3dddea58af5cddba72ad0153e2076b30611d
Thiago <tsbressan@gmail.com>
2021-06-23 14:54:50 -0300

    Files

---
+++ config.py
@@ -0,0 +1,19 @@
+
+#Configuration file 
+
+#algoritms-methods
+alg_meth = {'RF': 'Random Forest', 'GB':'Gradient Tree Boosting','AB':'AdaBoost'}
+
+#lithologys
+litho = {
+    10: 'Very fine sand/sandstone, Fine sand/sandstone, Medium sand/sandstone, Coarse sand/sandstone, Sand,Sand/Sandstone', 
+    11: 'Alternating sand/sandstone and mud/mudstone layers, Sandy clay/claystone, Sandy silt/siltstone', 
+    12: 'Alternating silt/siltstone and clay/claystone layers, Clayey silt/siltstone', 
+    13: 'Sand/sandstone-silt/siltstone-clay/claystone, Clayey sand/sandstone, Silty sand/sandstone', 
+    14: 'Silt/Siltstone, Silty clay/claystone, Clay/claystone, Clay, Silt, Mudstone', 
+    15: 'Calcareous silty clay/claystone, Calcareous silt/siltstone, Calcareous ooze, Chalk, Marlstone, Rudstone, Floatstone, Grainstone, Packstone, Wackestone, Boundstone, Limestone, Calcareous claystone'
+}
+
+#directories
+dir_main = ""
+dataset = dir_main + "datasets/"
